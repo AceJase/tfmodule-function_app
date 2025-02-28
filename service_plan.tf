4f28af984421437e6d21602354b887879f4fe629
Jason White <jason.white@arnoldclark.com>
2025-02-28 19:23:58 +0000

    use azure naming conventions

--- service_plan.tf
+++ service_plan.tf
@@ -1,6 +1,6 @@
 resource "azurerm_service_plan" "func_service_plan" {
   for_each            = var.functions
-  name                = format("planmyfunc%s", each.key)
+  name                = format("func-%s", each.key)
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
   os_type             = "Windows"
